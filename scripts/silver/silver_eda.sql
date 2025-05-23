630195eea42d5d7023e082197bb648517fb599ff
Glory Odeyemi <gloryodeyemi.nike@gmail.com>
2025-05-22 23:05:28 -0400

    Loading silver.crm_prd_info

--- scripts/silver/silver_eda.sql
+++ scripts/silver/silver_eda.sql
@@ -28,3 +28,40 @@ FROM silver.crm_cust_info;
 
 SELECT DISTINCT cst_marital_status
 FROM silver.crm_cust_info;
+
+SELECT * FROM silver.crm_cust_info;
+
+/*
+Data Quality Check - crm_prod_info table
+*/
+
+-- Check for nulls or duplicates in primary key
+-- Expectation: no result
+SELECT prd_id, COUNT(*)
+FROM silver.crm_prd_info
+GROUP BY prd_id
+HAVING COUNT(*) > 1 OR prd_id IS NULL;
+
+-- Check for unwanted spaces
+-- Expectation: no result
+-- Repeat for string features
+SELECT prd_nm
+FROM silver.crm_prd_info
+WHERE prd_nm != TRIM(prd_nm);
+
+-- Check for nulls or negative numbers
+-- Expectation: No results
+SELECT prd_cost
+FROM silver.crm_prd_info
+WHERE prd_cost < 0 OR prd_cost IS NULL;
+
+-- Check for data consistency
+SELECT DISTINCT prd_line
+FROM silver.crm_prd_info;
+
+-- Check for invalid date orders
+SELECT *
+FROM silver.crm_prd_info
+WHERE prd_end_dt < prd_start_dt;
+
+SELECT * FROM silver.crm_prd_info;
--- scripts/silver/silver_quality_check.sql
+++ scripts/silver/silver_quality_check.sql
@@ -22,3 +22,36 @@ FROM bronze.crm_cust_info;
 
 SELECT DISTINCT cst_marital_status
 FROM bronze.crm_cust_info;
+
+/*
+Data Quality Check - crm_prod_info table
+*/
+
+-- Check for nulls or duplicates in primary key
+-- Expectation: no result
+SELECT prd_id, COUNT(*)
+FROM bronze.crm_prd_info
+GROUP BY prd_id
+HAVING COUNT(*) > 1 OR prd_id IS NULL;
+
+-- Check for unwanted spaces
+-- Expectation: no result
+-- Repeat for string features
+SELECT prd_nm
+FROM bronze.crm_prd_info
+WHERE prd_nm != TRIM(prd_nm);
+
+-- Check for nulls or negative numbers
+-- Expectation: No results
+SELECT prd_cost
+FROM bronze.crm_prd_info
+WHERE prd_cost < 0 OR prd_cost IS NULL;
+
+-- Check for data consistency
+SELECT DISTINCT prd_line
+FROM bronze.crm_prd_info;
+
+-- Check for invalid date orders
+SELECT *
+FROM bronze.crm_prd_info
+WHERE prd_end_dt < prd_start_dt;
