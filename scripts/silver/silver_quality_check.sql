b9770b02b67d5403baebcaf304c466bee5c47357
Glory Odeyemi <gloryodeyemi.nike@gmail.com>
2025-05-26 16:17:23 -0400

    Transforming and Loading silver.crm_sales_details

--- scripts/silver/silver_quality_check.sql
+++ scripts/silver/silver_quality_check.sql
@@ -55,3 +55,64 @@ FROM bronze.crm_prd_info;
 SELECT *
 FROM bronze.crm_prd_info
 WHERE prd_end_dt < prd_start_dt;
+
+/*
+Data Quality Check - crm_sales_details table
+*/
+-- Check for unwanted spaces
+-- Expectation: no result
+-- Repeat for string features
+SELECT
+    sls_ord_num,
+	sls_prd_key,
+	sls_cust_id,
+	sls_order_dt,
+	sls_ship_dt,
+    sls_due_dt,
+	sls_sales,
+	sls_quantity,
+	sls_price
+FROM bronze.crm_sales_details
+WHERE sls_ord_num != TRIM(sls_ord_num);
+
+-- Check quality of product key from silver product info
+-- Repeat for customer id
+-- Expectation: no result
+SELECT
+    sls_ord_num,
+	sls_prd_key,
+	sls_cust_id,
+	sls_order_dt,
+	sls_ship_dt,
+    sls_due_dt,
+	sls_sales,
+	sls_quantity,
+	sls_price
+FROM bronze.crm_sales_details
+WHERE sls_prd_key NOT IN (SELECT prd_key FROM silver.crm_prd_info);
+
+-- Check for invalid dates
+-- Repeat for all dates
+SELECT 
+    NULLIF(sls_order_dt,0) sls_order_dt
+FROM bronze.crm_sales_details
+WHERE sls_order_dt <= 0 
+    OR LEN(sls_order_dt) != 8
+    OR sls_order_dt > 20500101
+    OR sls_order_dt < 19000101;
+
+SELECT *
+FROM bronze.crm_sales_details
+WHERE sls_order_dt > sls_ship_dt
+    OR sls_order_dt > sls_due_dt;
+
+-- Check for data consistency in sales, price, and quantity
+SELECT DISTINCT
+	sls_sales,
+	sls_quantity,
+	sls_price
+FROM bronze.crm_sales_details
+WHERE sls_sales != sls_quantity * sls_price
+    OR sls_sales IS NULL OR sls_quantity IS NULL or sls_price IS NULL
+    OR sls_sales <= 0 OR sls_quantity <= 0 or sls_price <= 0
+ORDER BY sls_sales, sls_quantity, sls_price;
